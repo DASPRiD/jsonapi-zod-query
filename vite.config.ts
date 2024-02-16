1d09a0475d44431096af3589ee2da29c0b268fd2
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2024-02-16 16:08:19 +0100

    fix(build): set filename of build to index.js

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         sourcemap: true,
         lib: {
             entry: resolve(__dirname, "src/index.ts"),
-            name: "index",
+            fileName: "index",
             formats: ["es"],
         },
         rollupOptions: {
